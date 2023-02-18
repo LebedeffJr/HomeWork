8ea214d548be1a0122e9fbfec002154e0884115b
LebedeffJr <Lebedeffjr@gmail.com>
2023-02-18 08:25:07 +0300

    Assignment2

--- ClassHomeWork.py
+++ ClassHomeWork.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -15,7 +25,17 @@ class Mentor:
 
 
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def average_grade(self):
+        for avrg in self.grades.values():
+            all_grades = None
+            all_grades += avrg
+        all_grades / len(self.grades.values)
+        return
+
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
@@ -26,8 +46,25 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+Tony = Lecturer('Tony', 'Stark')
+Barry = Student('Barry','Alen','man')
+Banner = Reviewer('Bruce', 'Banner')
+
+Banner.courses_attached += ['Python']
+Tony.courses_attached += ['Python']
+Barry.finished_courses += ['Git']
+Barry.courses_in_progress += ['Python']
+
+Banner.rate_hw(Barry, 'Python', 10)
+Banner.rate_hw(Barry, 'Python', 8)
+Banner.rate_hw(Barry, 'Python', 9)
+
+Barry.rate_lecturer(Tony, 'Python', 10)
+Barry.rate_lecturer(Tony, 'Python', 7)
+Barry.rate_lecturer(Tony, 'Python', 9)
+
+
+
+print(Barry.grades)
+print(Tony.grades)
